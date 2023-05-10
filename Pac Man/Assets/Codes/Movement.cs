082f93b09eac894ed64d106c4c35c63b7a713867
Chelsea <126628086+Bamber22@users.noreply.github.com>
2023-05-10 17:58:03 -0400

    derftghyj
    frtghyjukil

--- Pac Man/Assets/Codes/Movement.cs
+++ Pac Man/Assets/Codes/Movement.cs
@@ -2,24 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public abstract class Movement : MonoBehaviour
 {
     public float speed;
     public Vector2 initialDirection;
     public LayerMask obstacleLayer;
 
     protected Rigidbody2D rb;
+    [SerializeField]
     protected Vector2 direction;
     protected Vector2 nextDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+        rb.MovePosition(position + translation);
+        Debug.Log(translation+translation);
     }
 }
