1e217abcd1f89afc506f82544ba43bf6ca19ca01
Chelsea <markhamonca@codeninjas.com>
2023-12-17 13:44:16 -0500

    hrgsd
    gevdzfvs

--- Tetris/Assets/Scripts/Tetromino.cs
+++ Tetris/Assets/Scripts/Tetromino.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Tetromino : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private float previousTime;
+    public float fallTime = 0.8f;
+   
     // Update is called once per frame
     void Update()
     {
+        float tempTime = fallTime;
+
+       if (Input.GetKeyUp(KeyCode.LeftArrow))
+       {
+            transform.position += Vector3.left;
+       }
+
+       if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+        }
         
+
+       if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            tempTime = tempTime/10;
+        }
+       if (Time.time - previousTime > tempTime)
+        {
+            transform.position += Vector3.down;
+            previousTime = Time.time;
+        }
     }
 }
